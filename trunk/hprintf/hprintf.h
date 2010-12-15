02b581955ad5e8fb31f88695468c4f0f77b3dea5
paccogf <paccogf@0e13b4a2-004e-0410-a747-013e438b3a4b>
2010-12-15 11:09:33 +0000

    Added items remotely
    D:\virus\CodecPack\0A0A8E5D245C2373DA9AD250CBB39C4F\hprintf

---
+++ trunk/hprintf/hprintf.h
@@ -0,0 +1,50 @@
+#ifndef _HPRINTF_H_
+#define _HPRINTF_H_
+#include <stdio.h>
+
+int hprintf(char * format)
+{
+int i;
+static char _default_indent_char_ = '\t';  // TIENE QUE SER UN CARACTER (DE MOMENTO)
+static int _indentation_level_ = 0;
+va_list lpvar; 
+int indentation_delta = 0;
+
+
+
+    //
+    // MIRAR SI SE ESPECIFICO INDENTACION
+    //
+    if((format[0] == '[') && (format[2] == ']'))
+    {
+        if(format[1] == '+')    // "[+]..."
+        {
+            indentation_delta = 1;        
+        }
+        else if((format[1] == '-') && (_indentation_level_ > 0)) // "[-]..."
+        {
+            indentation_delta = -1;        
+        }
+    }
+
+    
+    if(indentation_delta < 0) _indentation_level_--;
+    
+    // ESCRIBIR CARACTER DE INDENTACION LAS VECES QUE HAGA FALTA
+    for(i=0; i < _indentation_level_; i++)
+    {
+        putchar(_default_indent_char_);
+    }
+
+    if(indentation_delta > 0) _indentation_level_++; // SI SE INCREMENTA LA INDENTACION, HACERLO EFECTIVO TRAS IMPRIMIR INDENTACION
+
+    va_start(lpvar, format);
+    i=vprintf(format, lpvar);
+    va_end(lpvar);
+
+
+
+return _indentation_level_;
+}
+
+#define
